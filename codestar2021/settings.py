51aeb38d8a8d33038bf810e553ec003aca42f682
legenduzair <uzairoomer@live.com>
2022-02-17 21:08:22 +0000

    Deployment

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2021-uzair.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['codestar2021.heroku.com', 'localhost']
 
 
 # Application definition
